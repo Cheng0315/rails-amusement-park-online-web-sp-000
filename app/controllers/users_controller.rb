1e5ec707b228b417942824ee22e6b46fb9277fd9
Cheng Vue <csalvue@gmail.com>
2019-02-04 17:47:19 +0000

    Automatically backed up by Learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save && !@user.admin
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    elsif @user.save && @user.admin
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :happiness, :nausea, :admin, :tickets, :password, :height)
+  end
+
+end
